af1621a33f8f966434857a029a75130bc19861fd
Ilya <IlyaDuginov1997@gmail.com>
2022-02-22 01:40:50 +0300

    registration, activate-link

--- service/user-service.js
+++ service/user-service.js
@@ -14,7 +14,7 @@ class UserService {
         const hashPassword = await bcrypt.hash(password, 3)
         const activateLink = uuid.v4()
         const user = await UserModel.create({email, password: hashPassword, activateLink})
-        await mailService.sendActivationMail(email, activateLink)
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activateLink}`)
 
         const userDto = new UserDto(user)
         const tokens = tokenService.generateTokens({...userDto})
