2f911da51519448778992f231ca2f5ac5ece22a2
e.makaruk <e.makaruk>
2022-02-21 19:12:54 +0300

    Initial commit

---
+++ controllers/user-controller.js
@@ -0,0 +1,51 @@
+const userService = require('../service/user-service')
+
+class UserController {
+    async registration(req, res, next) {
+        try {
+            const {email, password} = req.body
+            const userData = await userService.registration(email, password)
+            res.cookie(`refreshToken`, userData.refreshToken, {maxAge: 30*24*60*60*1000, httpOnly: true})
+            return res.json(userData)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    async login(req, res, next) {
+        try {
+
+        } catch (err) {
+
+        }
+    }
+    async logout(req, res, next) {
+        try {
+
+        } catch (err) {
+
+        }
+    }
+    async activate(req, res, next) {
+        try {
+
+        } catch (err) {
+
+        }
+    }
+    async refresh(req, res, next) {
+        try {
+            res.json(['Ilya, Gleb, Nastya'])
+        } catch (err) {
+
+        }
+    }
+    async getUsers(req, res, next) {
+        try {
+            res.json(['123', '456'])
+        } catch (err) {
+
+        }
+    }
+}
+
+module.exports = new UserController()
